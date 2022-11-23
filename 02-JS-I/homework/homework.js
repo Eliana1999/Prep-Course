7a6e3814df1dcc111d9cb4810215cd4771475446
eliana1999 <eliana_gasparri@hotmail.com>
2022-11-23 17:20:16 -0300

    hh

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,14 +195,14 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return "hello world"  + "!" ;  
+  return str  + "!" ;  
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinado = nombre + "Soy" + apellido + "Henry" ;
+  var combinado = nombre + apellido ;
   return combinado ;
 }
 
@@ -210,7 +210,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + "Martin" + "!" ;
+  return nombre + "Hola" + "Martin" + "!" ;
 
 }
 
